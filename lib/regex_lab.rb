76b937d19785b738bf60760b3ddde600bae94292
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-10 15:24:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -29,6 +29,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-reg = \(*[0-9]{3}\)*[\s]*[0-9]{3}[-\s]*[0-9]{4}
+reg = /\(*[0-9]{3}\)*[\s]*[0-9]{3}[-\s]*[0-9]{4}/
 return phone.match(reg)
 end
